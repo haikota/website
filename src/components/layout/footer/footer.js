e8599342744bd84e06aff5fae5b7327661ff3b76
hailk <hailk@chainos.vn>
2018-11-13 06:05:59 +0700

    fix

---
+++ src/components/layout/footer/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class footer extends React.Component{
+    render(){
+        return(
+            <div className="footer">footer</div>
+        )
+    }
+}
---
+++ src/components/layout/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {Row, Col} from 'react-bootstrap';
+
+export default class header extends React.Component{
+    render(){
+        return(
+            <div className="main_header">
+                <div className="container">header</div>
+            </div>
+        )
+    }
+}
